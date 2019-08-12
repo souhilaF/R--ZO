a8995d2ac9e6ccd3430b10e0b40d3ed9e82dec22
souhilaF <50913274+souhilaF@users.noreply.github.com>
2019-08-12 19:48:34 +0200

    Update simul_evenements_Saturnin.R

--- simul_evenements_Saturnin.R
+++ simul_evenements_Saturnin.R
@@ -20,19 +20,24 @@ for (p in val_p) {
   for (sim in 1:100) { 
     
     print(paste("p=", p,", sim = ",sim))
-    file_name_data = paste('res_simu/datasim/datasim_n',n,'_p',p,'_sim',sim,'.Rdata',sep='')
-    load(file_name_data)
-    weights = lweights_gaussian(datasim$Y)
-    score = edge.prob(weights, log = TRUE) 
+    file_name_result = paste('res_simu/ressimSaturnin/ressimSaturnin_n',n,'_p',p,'_sim',sim,'.Rdata',sep='')
+    if(!file.exists(file_name_result)){
+      
+       file_name_data = paste('res_simu/datasim/datasim_n',n,'_p',p,'_sim',sim,'.Rdata',sep='')
+       load(file_name_data)
+       weights = lweights_gaussian(datasim$Y)
+       score = edge.prob(weights, log = TRUE) 
     #il faut faire un logit ou un probit sur les scores car production de NA
     #on a des scores superieur a 1 et =0 revoir la simulation de données
-    file_name_result = paste('res_simu/ressimSaturnin/ressimSaturnin_n',n,'_p',p,'_sim',sim,'.Rdata',sep='')
-    save(score,file = file_name_result)
-    output <- VEM(S = score,K, niter=1000, epsilon_tau=1e-4, epsilon_eta = 1e-4,verbose = FALSE)
-    save(score,output,file=file_name_result)
-    datasim = c(); paramsim = c(); output = c(); score = c()
-    
+      
+
+       output <- VEM(S = score,K, niter=1000, epsilon_tau=1e-4, epsilon_eta = 1e-4,verbose = FALSE)
+       save(score,output,file=file_name_result)
+       datasim = c(); paramsim = c(); output = c(); score = c()
+      }
+    }
   }
-        }
+
+
     
     
